c087a5bd78ca6726c3d2b0a295813b029d85581c
Sineth Thilanka Gunarathna <sineththilanka0754@gmail.com>
2023-12-07 20:04:01 +0530

    change number 1

--- Front-end/GamageRecuiters-app/src/App.jsx
+++ Front-end/GamageRecuiters-app/src/App.jsx
@@ -11,20 +11,16 @@ import Register from "./pages/Admin Register/Admin_register";
 import Panel from "./pages/Admin Panel/Admin_panel";
 
 import Footer from "./pages/Components/Footer";
-//import './App.css';
-
-
-=========
 import Jobvacancy from "./pages/Job-vacancy/Jobvacancy";
 import Clientdiaries from "./pages/Client Diaries/Clientdiaries";
 import Ourjourney from "./pages/Our Journey/Ourjourney";
->>>>>>>>> Temporary merge branch 2
+//import './App.css';
 function App() {
 
   return (
     <>
       <BrowserRouter>
-<<<<<<<<< Temporary merge branch 1
+
         <Routes>
           {/* client side */}
           <Route path="/" element={<Home></Home>}> </Route>
@@ -35,20 +31,13 @@ function App() {
           <Route path="/register" element={<Register></Register>}> </Route>
           <Route path="/panel" element={<Panel></Panel>}> </Route>
 
-
-        </Routes>
-
-=========
-      <Routes>
-             <Route path="/" element={<Home></Home>}> </Route>
-             <Route path="/Contactus" element={<Contactus></Contactus>}> </Route>
-             <Route path="/Clientdiaries" element={<Clientdiaries></Clientdiaries>}> </Route>
-             <Route path="/Jobvacancy" element={<Jobvacancy></Jobvacancy>}> </Route>
-             <Route path="/Ourjourney" element={<Ourjourney></Ourjourney>}> </Route>
+          <Route path="/Clientdiaries" element={<Clientdiaries></Clientdiaries>}> </Route>
+          <Route path="/Jobvacancy" element={<Jobvacancy></Jobvacancy>}> </Route>
+          <Route path="/Ourjourney" element={<Ourjourney></Ourjourney>}> </Route>
       
       </Routes>
       
->>>>>>>>> Temporary merge branch 2
+
       </BrowserRouter>
       <Footer/>
     </>
